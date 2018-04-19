77aa11f46883423864de819c3c5af15720eabc13
Harry <harryhum22@gmail.com>
2018-04-19 18:13:12 -0400

    game save and load

--- lib/main.rb
+++ lib/main.rb
@@ -1,4 +1,5 @@
-#load dictionary
+require 'yaml'
+
 def load_dictionary
     words = File.readlines "5desk.txt"
 
@@ -10,45 +11,50 @@ def load_dictionary
     end
 end
 
-#select 5-12 letter word
 def select_word
     @secret_word = @dictionary[rand(@dictionary.length - 1)]
 end
 
-#update word board
-def update_word_board
+def initialize_word_board
     index = 0
     @secret_word.each_char do |letter|
         #word_board[index] - [0] = letter of secret word, [1] = visible to player (y/n), [2] = character shown to player
-        @word_board[:index] = [letter, "n", "_"]
+        @word_board[index] = [letter, "n", "_"]
         index += 1
-        if @guess != "" && @guess = @word_board[:index][0]
-            @word_board[:index][1] = "y"
-            @good_guess = true
-        elsif @guess != "" && @guess != @word_board[:index][0] 
-            @good_guess = false
-            @mistakes += 1
-        end
     end
 end
 
-#return word visible to player
+def check_guess
+    @good_guess = false
+    index = 0
+    @secret_word.each_char do |letter|
+        if @guess.eql?@word_board[index][0]
+            @word_board[index][1] = "y"
+            @good_guess = true
+        end
+        index += 1
+    end
+    if @good_guess == false
+        @mistakes += 1
+        @incorrect_letters << @guess
+    end
+end
+
 def show_word_board
     print "Secret word: "
     index = 0
     @secret_word.each_char do |letter|
-        if @word_board[:index][1] == "y"
-            @word_board[:index][2] = @word_board[:index][0]
+        if @word_board[index][1] == "y"
+            @word_board[index][2] = @word_board[index][0]
         end
-        print @word_board[:index][2]
+        print @word_board[index][2]
         print " "
         index += 1
     end
-    puts
 end
 
-#draw hangman board (hanger, man, number of incorrect guesses, word board, used letters)
 def draw_board
+    puts
     puts        "  [-----|"
     if @mistakes == 0
         puts    "        |"
@@ -76,57 +82,120 @@ def draw_board
     puts
     show_word_board
     puts
-    puts "Incorrect letters: "
+    print "Incorrect letters: " + @incorrect_letters.to_s
+    puts
     puts "Wrong guesses left: #{5 - @mistakes}"
 end
 
-#take guess
 def get_guess
-    print "Suggest a letter: "
+    print "Suggest a letter or type \"save\" to save the game: "
     @guess = gets.chomp.downcase
+    if @guess == "save"
+        save_game
+    end
     while @guess.length == 0 || @guess.length > 1 do
-        print "That is not a valid letter"
+        puts "That is not a valid letter."
+        print "Suggest a letter: "
         @guess = gets.chomp.downcase
     end
 end
 
-#check win/lose
-def game_over
-    index = 0 
+def win?
+    win = true
+    index = 0
     @secret_word.each_char do |letter|
-        if @word_board[:index][1] == "y"
-            @win == true
+        if @word_board[index][1] == "n"
+            win = false
         end
         index += 1
     end
+    return win
+end
 
-    if @mistakes == 5
+def lose?
+    @mistakes == 5 ? true : false
+end
+
+def game_over
+    if win? == true
+        draw_board
+        puts "You won!"
         true
-    elsif @win == true
+    elsif lose? == true
+        draw_board
+        puts "You lost! The secret word was " + @secret_word + "."
         true
-    else 
+    else
         false
     end
-end 
-
-#execute turn actions (guess letter, update board, check win/lose)
-def execute_turn
-    draw_board
-    get_guess
-    update_word_board
-    game_over
 end
 
 def new_game
-    #initialize class variables
     @dictionary = []
     @word_board = {}
     @secret_word = ""
     @guess = ""
-    @good_guess = false
+    @incorrect_letters = []
     @mistakes = 0
-    load_dictionary
-    select_word
-    update_word_board
-    game_over
+    print_instructions
+    prompt_game_load
+end
+
+def save_game
+    game_data = [@word_board, @secret_word, @incorrect_letters, @mistakes]
+    yaml = YAML::dump(game_data)
+    save_file = File.new("save_file.yaml", "w+")
+    save_file.write(yaml)
+    puts "The current game has been saved."
+    exit
+end
+
+def load_game
+    game_data = YAML::load_file("save_file.yaml")
+    if game_data == false
+        puts "No saved games found."
+        puts "Starting new game."
+        load_dictionary
+        select_word
+        initialize_word_board
+    else
+        @word_board = game_data[0]
+        @secret_word = game_data[1]
+        @incorrect_letters = game_data[2]
+        @mistakes = game_data[3]
+        puts "The previous game has been successfully loaded."
+    end
+    File.new("save_file.yaml", "w+")
+end
+
+def prompt_game_load
+    print "Would you like to load the last saved game? (y/n): "
+    answer = gets.chomp.downcase
+
+    if answer == "y"
+        load_game
+    elsif answer == "n"
+        load_dictionary
+        select_word
+        initialize_word_board
+    else
+        print "Please enter (y/n): "
+        answer = gets.chomp.downcase
+    end
+end
+
+def print_instructions
+    puts "The fate of the man on the noose is in your hands..."
+    puts "Guess all the letters of the secret word before making 5 mistakes."
+
+    puts "You can save the game at any time."
+end
+
+new_game
+while (!game_over) do
+    draw_board
+    get_guess
+    check_guess
+    win?
+    lose?
 end
